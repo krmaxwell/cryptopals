1ada45d5f14a2ea7fbbb38e4a32291946dbddef9
Kyle Maxwell <kylem@xwell.org>
2013-07-18 22:24:26 -0500

    Solved problem 1

--- problem1.py
+++ problem1.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 
 import sys
+import string
+import base64
 
-inputhex = argv[1]
-
+data = long(sys.argv[1],base=16)
+hexdat = base64.b16decode(hex(data)[2:-1],casefold=True)
+output = base64.b64encode(hexdat)
+print output
 
