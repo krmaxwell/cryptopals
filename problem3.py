eae5a65c81534052297fba9d60f6f59bf59e6f25
Kyle Maxwell <krmaxwell@gmail.com>
2013-07-21 23:05:34 +0000

    XOR works, identifies candidate strings but cannot distinguish between them

--- problem3.py
+++ problem3.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python
 
 import sys
+import binascii
 
 ciphertext = long(sys.argv[1],base=16)
+cipherdata = bytearray(binascii.unhexlify('%x' % ciphertext))
 
 for key in xrange(0,256):
-  fullkey = ke
+	cleardata=bytearray()
+	for i in xrange(0,len(cipherdata)):
+		data = cipherdata[i] ^ key
+		if data in range(32,128):
+			cleardata.append(cipherdata[i] ^ key)
+		else:
+			break
+	if len(cleardata) == len(cipherdata):
+		print cleardata
