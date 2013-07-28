5a67270550e31cd61345718f9f0104e4eddb73c5
Kyle Maxwell <krmaxwell@gmail.com>
2013-07-28 16:42:42 +0000

    Started library, need to clean up strxor/xorstr later

---
+++ cryptokrm.py
@@ -0,0 +1,24 @@
+# An assortment of functions useful in crypto
+
+def strxor(s1, s2):
+    if len(s1)==len(s2):
+        return ''.join(chr(ord(a) ^ ord(b)) for a,b in zip(s1,s2))
+    return False
+
+def xorstr(message, key):
+    if len(key) < len(message):
+        ekey = (key * (len(message)/len(key)+1))[:len(message)]
+    else:
+        ekey = key
+
+    return strxor(message,ekey)
+
+def hamming_dist(s1,s2):
+    s = strxor(s1,s2)
+    bits = 0
+
+    if s: # they are in fact the same length
+        for c in s:
+            bits += sum(bit == '1' for bit in bin(ord(c))[2:]) 
+    else:
+        return False
