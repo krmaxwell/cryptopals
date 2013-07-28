4fa89c7ad038ae8e0afdf80131583c39d05b8e55
Kyle Maxwell <krmaxwell@gmail.com>
2013-07-28 05:35:25 +0000

    Herp derp, just learned about codecs

--- problem1.py
+++ problem1.py
@@ -4,16 +4,11 @@ import sys
 import base64
 
 # first convert the string to actual binary data
-data = long(sys.argv[1],base=16)
-
-# hex() with a long type arg returns something of the form "0xc0ffee"
-# so we slice out the part of hex() output we want
-# this is effectively base16 per RFC 3548
-hexdat = base64.b16decode(hex(data)[2:-1],casefold=True)
+data = sys.argv[1].decode('hex')
 
 # hexdat is now a string containing the same bytes as data
 # we did this becase b64encode() is annoying about input types
-output = base64.b64encode(hexdat)
+output = base64.b64encode(data)
 print output
 
 # go back the other way: turn base64 into hex
