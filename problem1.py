3cff5d367d3512f33ca0a4d4c7c8059cb2bfda7a
Kyle Maxwell <krmaxwell@gmail.com>
2013-07-28 00:04:01 -0500

    Commented problem1.py so I know why I did it this way later

--- problem1.py
+++ problem1.py
@@ -4,8 +4,16 @@ import sys
 import string
 import base64
 
+# first convert the string to actual binary data
 data = long(sys.argv[1],base=16)
+
+# hex() with a long type arg returns something of the form "0xc0ffee"
+# so we slice out the part of hex() output we want
+# this is effectively base16 per RFC 3548
 hexdat = base64.b16decode(hex(data)[2:-1],casefold=True)
+
+# hexdat is now a string containing the same bytes as data
+# we did this becase b64encode() is annoying about input types
 output = base64.b64encode(hexdat)
 print output
 
