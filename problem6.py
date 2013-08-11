9bae8c5eb7c5c80773b77097f17e033c64599998
Kyle Maxwell <krmaxwell@gmail.com>
2013-08-11 22:16:02 +0000

    Break up ciphertext into blocks and transpose

--- problem6.py
+++ problem6.py
@@ -7,16 +7,31 @@ from cryptokrm import *
 with open('data6.txt','rb') as f:
     message = f.read()
 
+# decode into ciphertext
 ctext = base64.b64decode(message)
+print "Ciphertext:\n",ctext.encode('hex')
+print "----------"
 
 min_k,min_d = 0,1000
 
-for k in xrange(3,65): 
-    d1 = float(hamming_dist(ctext[0:k-1],ctext[k:k*2-1])) / k
-    d2 = float(hamming_dist(ctext[k:k*2-1],ctext[k*2:k*3-1])) / k
-    d3 = float(hamming_dist(ctext[k*2:k*3-1],ctext[k*3:k*4-1])) / k
-    d = (d1+d3+d2)/3
+for k in xrange(4,65): 
+    # Hamming dist between block 1 and block 2
+    d1 = float(hamming_dist(ctext[0:k],ctext[k:k*2])) / k
+    # Hamming dist between block 2 and block 3
+    d2 = float(hamming_dist(ctext[k:k*2],ctext[k*2:k*3])) / k
+    # Hamming dist between block 3 and block 4
+    d3 = float(hamming_dist(ctext[k*2:k*3],ctext[k*3:k*4])) / k
+    d = (d1+d2+d3)/3
     if d < min_d:
         min_k, min_d = k,d
 
-print "Probable keysize is %d with Hdist %f" % (min_k, min_d)
+print "Probable keysize is %d with dist %f" % (min_k, min_d)
+k = min_k
+
+# now split cipher
+btext = [0]*k
+
+for i in xrange(0,k):
+    btext[i] = ctext[i::k]
+
+print [b.encode('hex') for b in btext]
