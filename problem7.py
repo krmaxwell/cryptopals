f3fb2b1eec1709b0c9ffa56a6a0c7fbe64b82b18
Kyle Maxwell <krmaxwell@gmail.com>
2013-08-11 19:17:59 +0000

    Solved problem 7

---
+++ problem7.py
@@ -0,0 +1,8 @@
+import base64
+from Crypto.Cipher import AES
+
+with open('data7.txt','rb') as f:
+  s = f.read()
+
+cipher = AES.new('YELLOW SUBMARINE',AES.MODE_ECB)
+print cipher.decrypt(base64.b64decode(s))
