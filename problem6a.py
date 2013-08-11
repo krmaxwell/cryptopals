8f6712bb1b9682910e0d5f6ae6fe935e69db9a14
Kyle Maxwell <krmaxwell@gmail.com>
2013-08-11 23:48:06 +0000

    Split problem6

--- problem6a.py
+++ problem6a.py
@@ -9,29 +9,22 @@ with open('data6.txt','rb') as f:
 
 # decode into ciphertext
 ctext = base64.b64decode(message)
-print "Ciphertext:\n",ctext.encode('hex')
-print "----------"
+#print "Ciphertext:\n",ctext.encode('hex')
+#print "----------"
 
 min_k,min_d = 0,1000
 
-for k in xrange(4,65): 
+for k in xrange(2,40): 
     # Hamming dist between block 1 and block 2
-    d1 = float(hamming_dist(ctext[0:k],ctext[k:k*2])) / k
+    d1 = float(hamming_dist(ctext[k*0:k*1],ctext[k*1:k*2])) / k
     # Hamming dist between block 2 and block 3
-    d2 = float(hamming_dist(ctext[k:k*2],ctext[k*2:k*3])) / k
+    d2 = float(hamming_dist(ctext[k*2:k*3],ctext[k*3:k*4])) / k
     # Hamming dist between block 3 and block 4
-    d3 = float(hamming_dist(ctext[k*2:k*3],ctext[k*3:k*4])) / k
+    d3 = float(hamming_dist(ctext[k*4:k*5],ctext[k*5:k*6])) / k
     d = (d1+d2+d3)/3
+    print k,d
     if d < min_d:
         min_k, min_d = k,d
 
+print "----------"
 print "Probable keysize is %d with dist %f" % (min_k, min_d)
-k = min_k
-
-# now split cipher
-btext = [0]*k
-
-for i in xrange(0,k):
-    btext[i] = ctext[i::k]
-
-print [b.encode('hex') for b in btext]
---
+++ problem6b.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import base64
+import sys
+
+from cryptokrm import *
+
+with open('data6.txt','rb') as f:
+    message = f.read()
+
+# user-defined keysize
+k = sys.argv[1]
+
+# now split cipher
+btext = [0]*k
+
+for i in xrange(0,k):
+    btext[i] = ctext[i::k]
+
+#print [b.encode('hex') for b in btext]
+#print btext[0].encode('hex')
+#print "----------"
+
+# just for testing: first k worth of bytes
+for key in xrange(0,256):
+    cleardata = ''
+    for i in xrange(0,len(btext[0])):
+        data = ord(btext[0][i]) ^ key
+        if data in xrange(32,128) or data == 10 or data == 13:
+            cleardata += chr(data)
+        else:
+            break
+    if len(cleardata) == len(btext[0]):
+        print key, cleardata
