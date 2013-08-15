b5d426d4ba36ca69b1d84082b19f6f2e30a04744
Kyle Maxwell <krmaxwell@gmail.com>
2013-08-14 20:51:10 -0500

    Calculate probable keysize using entire ciphertext

--- problem6a.py
+++ problem6a.py
@@ -9,22 +9,17 @@ with open('data/data6.txt','rb') as f:
 
 # decode into ciphertext
 ctext = base64.b64decode(message)
-#print "Ciphertext:\n",ctext.encode('hex')
-#print "----------"
 
 min_k,min_d = 0,1000
 
 for k in xrange(2,40): 
-    # Hamming dist between block 1 and block 2
-    d1 = float(hamming_dist(ctext[k*0:k*1],ctext[k*1:k*2])) / k
-    # Hamming dist between block 2 and block 3
-    d2 = float(hamming_dist(ctext[k*2:k*3],ctext[k*3:k*4])) / k
-    # Hamming dist between block 3 and block 4
-    d3 = float(hamming_dist(ctext[k*4:k*5],ctext[k*5:k*6])) / k
-    d = (d1+d2+d3)/3
+    dists = []
+    for n in xrange(len(ctext)/k-1):
+        dists.append(float(hamming_dist(ctext[k*n:k*(n+1)],ctext[k*(n+1):k*(n*2)]))/k)
+    d = sum(dists)
     print k,d
     if d < min_d:
         min_k, min_d = k,d
 
 print "----------"
-print "Probable keysize is %d with dist %f" % (min_k, min_d)
+print "Probable keysize is %d with dist %.4f" % (min_k, min_d)
