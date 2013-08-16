7d5f67cac73733d9e8a44cbf4b8550520f4ea040
Kyle Maxwell <krmaxwell@gmail.com>
2013-08-16 15:03:49 -0500

    Finished set 1 and submitted to Matasano

--- problem3.py
+++ problem3.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python
 
 import sys
-import binascii
-import re
+import operator
+
 from cryptokrm import *
 
 message = sys.argv[1].decode('hex')
 similarity = dict()
 
+fullkey = ''
+
 for key in xrange(0,256):
     candidate = xorstr(message,chr(key))
-    similarity[key] = cos_sim(english.values(),frequency(candidate).values())
-    if similarity[key] > 0.3:
-        print "%3d %.4f" % (key, similarity[key])
+    similarity[key] = cos_sim(asciifreq.values(),frequency(candidate).values())
+
+best = max(similarity.iteritems(), key=operator.itemgetter(1))[0]
+print best
+
+print xorstr(message,chr(best))
--- problem6b.py
+++ problem6b.py
@@ -19,7 +19,7 @@ btext = [0]*k
 
 fullkey = ''
 
-for i in xrange(0,k):
+for i in xrange(k):
     btext[i] = ctext[i::k]
     similarity=dict()
     for key in xrange(0,256):
@@ -28,9 +28,15 @@ for i in xrange(0,k):
             cleardata += chr(ord(btext[i][j]) ^ key)
         similarity[key] = cos_sim(asciifreq.values(),frequency(cleardata).values())
     best = max(similarity.iteritems(), key=operator.itemgetter(1))[0]
+    print "Position %x best match is %x with cossim %.3f" % (i, best, similarity[best])
+    if (i == 3) or (i == 6) or (i == 27):
+        for j in xrange(256):
+            if similarity[j] > 0.3:
+                print "Alternate match %x with cossim %.3f" % (j, similarity[j])
     fullkey += chr(best)
     
 print fullkey
+fullkey = "Terminator X: Bring the noise"
+print fullkey
 print fullkey.encode('hex')
-
 print xorstr(ctext,fullkey)
