b65cfea2c8bd2e30126402010b26bcc3481bbdf2
Kyle Maxwell <kylem@xwell.org>
2013-08-24 01:02:45 +0000

    Solved problem 11

--- problem11.py
+++ problem11.py
@@ -1,11 +1,17 @@
+from cryptokrm import *
+
 def chunks(l, n):
     for i in xrange(0, len(l), n):
         yield l[i:i+n]
 
-with open('gettysburg-oracle.txt','rb') as f:
-    cipher_texts = f.readlines()
-
 data = []
+cipher_texts = []
+plain_text = ' ' * 1024
+
+# Generate chosen plaintexts
+for i in xrange(16):
+    cipher_texts.append(encryption_oracle(plain_text))
+
 # first break each ciphertext into blocks of 16 bytes (128 bits)
 for t in cipher_texts:
     data.append(list(chunks(t,16)))
--- problem11a.py
+++
@@ -1,7 +0,0 @@
-from cryptokrm import *
-
-with open('gettysburg.txt','rb') as f:
-    s = f.read()
-
-for i in xrange(16):
-    print encryption_oracle(s).encode('hex')
