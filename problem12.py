a04ff31d7915f247ce1fe004687e9870b56f0e3a
Kyle Maxwell <kylem@xwell.org>
2013-08-24 21:33:17 -0500

    Decrypting first block successfully

--- problem12.py
+++ problem12.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import random
 import base64
 
@@ -7,16 +8,15 @@ from cryptokrm import *
 secret_data = base64.b64decode('Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK')
 
 key = os.urandom(16)
+cipher = AES.new(key,AES.MODE_ECB)
 decrypt = ''
 
 def chunks(l, n):
     for i in xrange(0, len(l), n):
         yield l[i:i+n]
 
-def test_func(message):
+def oracle(message):
     data = ''.join((message,secret_data))
-
-    cipher = AES.new(key,AES.MODE_ECB)
     data = pkcs7padding(data,16)
     return cipher.encrypt(data)
 
@@ -25,7 +25,7 @@ def detect_ecb():
     tempdict = dict()
     flag = False
 
-    ciphertext = test_func(plaintext)
+    ciphertext = oracle(plaintext)
     data = list(chunks(ciphertext,16))
 
     for block in data:
@@ -38,10 +38,10 @@ def detect_ecb():
     return flag
     
 # Step 1: Determine block size
-secret_len=len(test_func(''))
+secret_len=len(oracle(''))
 test_str = ''
 for i in xrange(64):
-    test_output = test_func(test_str)
+    test_output = oracle(test_str)
     if len(test_output) > secret_len:
         blocksize = len(test_output) - secret_len
         print "Block size is", blocksize
@@ -53,19 +53,17 @@ print "Function uses ECB?", detect_ecb()
 
 # Step 3: Craft input block 1 byte short
 # Step 4: Match result from test input to all possible vals for that byte
-# because the oracle will end up padding it otherwise
-for i in xrange(4):
-    test_block = 'A' * (blocksize-(i+1)) + decrypt
-    print test_block, len(test_block)
-    test_out = test_func(test_block)[:blocksize]
-
+for i in xrange(15):
     for c in xrange(256):
-        input_block = 'A' * (blocksize-(i+1)) + chr(c) + decrypt
-        print input_block, len(input_block)
-        test_result = test_func(input_block)[:blocksize]
-        if test_result == test_out:
-            print "Candidate character is %c - %03d" % (chr(c), c)
-            decrypt = chr(c) + decrypt
+        guess_block = 'A' * (blocksize-(i+1)) + decrypt + chr(c)
+        guess_result = oracle(guess_block)[:blocksize]
+        test_block = 'A' * (blocksize-(i+1))
+        test_result = oracle(test_block)[:blocksize]
+        #print guess_block.encode('hex'),len(guess_block),test_block.encode('hex'),len(test_block)
+        if guess_result == test_result:
+            sys.stdout.write(chr(c))
+            decrypt = decrypt + chr(c)
             break
 
-print decrypt
+print
+#print decrypt
