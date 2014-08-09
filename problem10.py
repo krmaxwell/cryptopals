46c1256661da6754c3556b5f410962bfa63b4a23
Kyle Maxwell <kmaxwell@verisign.com>
2014-08-08 23:24:00 -0700

    PEP8

--- problem10.py
+++ problem10.py
@@ -2,9 +2,9 @@ import base64
 
 from cryptokrm import *
 
-with open('data/data10.txt','rb') as f:
-  s = f.read()
+with open('data/data10.txt', 'rb') as f:
+    s = f.read()
 
 data = base64.b64decode(s)
 
-print cbcdecrypt(data,"YELLOW SUBMARINE")
+print cbcdecrypt(data, "YELLOW SUBMARINE")
