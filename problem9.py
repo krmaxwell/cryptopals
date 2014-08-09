25275dd66b10bde50b489ab9a7df55de95ed67e0
Kyle Maxwell <kmaxwell@verisign.com>
2014-08-08 23:15:04 -0700

    Solved problem 9

--- problem9.py
+++ problem9.py
@@ -2,6 +2,6 @@ import sys
 from cryptokrm import *
 
 message = sys.argv[1]
-blocklen = sys.argv[2]
+blocklen = int(sys.argv[2])
 print message.encode('hex')
-print pkcs7padding(message,key).encode('hex')
+print pkcs7padding(message, blocklen).encode('hex')
